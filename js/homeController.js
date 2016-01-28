c439b5cef77530a20f6b9b9419b0b571b8d0c538
Brandon O'Very <brovery@yahoo.com>
2016-01-28 10:11:51 -0700

    Got ngdialog working.

--- js/homeController.js
+++ js/homeController.js
@@ -11,6 +11,18 @@
         // grid (and its data) are in the service for persistence
         var hc = this;
         hc.myGrid = homeService.myGrid;
+        hc.openDialog = openDialog;
+        hc.openMe = openMe;
+
+        function openDialog (person) {
+            homeService.openDialog(person);
+        }
+
+        function openMe() {
+            console.log("OPEN ME!!!");
+        }
+
+
 
     }
 
--- js/homeService.js
+++ js/homeService.js
@@ -4,52 +4,38 @@
     angular.module('homeService', [])
         .service('homeService', homeService);
 
-    homeService.$inject = ['$http', 'uiGridConstants'];
+    homeService.$inject = ['$http', 'ngDialog'];
 
-    function homeService($http, uiGridConstants) {
+    function homeService($http, ngDialog) {
 
         // list everything
         var hs = this;
+        hs.openDialog = openDialog;
+
         //hs.myGrid = { data: undefined };
         hs.myGrid = {
             data: undefined,
             enableFiltering: true,
-            enableFullRowSelection: true,
+            //enableFullRowSelection: true,
             showColumnFooter: true,
             columnDefs: [
                 { field: 'name' },
-                { field: 'height' },
-                { field: 'mass', name: 'weight', cellTemplate: '<span>{{ row.entity.mass }} kg</span>'},
-                { field: 'hair_color' },
-                { field: 'speciesName' }
-            ],
-            columnDefsx: [
-                { field: 'name' },
-                { field: 'company' },
-                { field: 'email', name: 'emailAddress',
-                    cellTemplate: '<a class="text-input" ng-href="mailto:{{ row.entity.email }}" ng-click="$event.stopPropagation()">'
-                        + '{{ row.entity.email }}</a>' },
-                { field: 'phone' },
-                { field: 'balance', width: 120 },
-                { field: 'age', width: 70, aggregationType: uiGridConstants.aggregationTypes.avg },
-                { field: 'about', enableSorting: false,
-                    cellTooltip: function(row, col) {
-                        return row.entity.about;
-                    }
-                },
-                // unused fields from json file
-                //{ field: 'id' },
-                //{ field: 'guid' },
-                //{ field: 'isActive' },
-                //{ field: 'registration' },
-                //{ field: 'friends' },
-                //{ field: 'picture' },
-                //{ field: 'gender' },
-                //{ field: 'address' },
+                //{ field: 'height' },
+                //{ field: 'mass', name: 'weight', cellTemplate: '<span>{{ row.entity.mass }} kg</span>'},
+                //{ field: 'hair_color' },
+                { field: 'species', cellTemplate: '<button class="btn btn-info" ng-click="grid.appScope.hc.openDialog(row.entity)">More Info</button>' }
             ],
             paginationPageSizes: [25, 50, 75],
             paginationPageSize: 25
         };
+
+        function openDialog(person) {
+            ngDialog.open({ plain: true, template: `<div>Name: ${person.name}</div>
+            <div>Height: ${person.height} cm</div>
+            <div>Weight: ${person.mass} kg</div>
+            <div>Hair Color: ${person.hair_color}</div>
+            <button class="btn btn-primary" ng-click="closeThisDialog()">OK</button>` });
+        }
 
         getData().then(function(data){
             hs.myGrid.data = data;
@@ -59,12 +45,12 @@
         function getData() {
             return $http.get('http://swapi.co/api/people')
                 .then(function (response) {
-                    response.data.results.forEach(function (row) {
-                        $http.get(row.species[0]).then(function (resp) {
-                            row.speciesName = resp.data.name;
-                        });
-                    });
-                    console.log(response.data.results[0]);
+                    //response.data.results.forEach(function (row) {
+                    //    $http.get(row.species[0]).then(function (resp) {
+                    //        row.speciesName = resp.data.name;
+                    //    });
+                    //});
+                    //console.log(response.data.results[0]);
                     return response.data.results;
                 });
         }
